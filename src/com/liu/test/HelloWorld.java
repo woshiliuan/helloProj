f80cc75abbb2dc7c242bc19a4bd0f9fc74c761f1
robin <514367261@qq.com>
2018-09-20 07:03:26 +0800

    Update HelloWorld.java

--- src/com/liu/test/HelloWorld.java
+++ src/com/liu/test/HelloWorld.java
@@ -7,15 +7,10 @@ class B extends A {
 }
 public class HelloWorld {
     public static void main(String[] args) {
+        System.out.println("testtest");
         A a = new A();
         B b = new B();
         A ba = new B();
-        System.out.println("2-------------");
-        System.out.println(a.getClass().isAssignableFrom(A.class));
-        System.out.println(b.getClass().isAssignableFrom(B.class));
-        System.out.println(a.getClass().isAssignableFrom(B.class));
-        System.out.println(b.getClass().isAssignableFrom(A.class));
-        System.out.println(ba.getClass().isAssignableFrom(A.class));
         System.out.println("testtest");
 
     }
