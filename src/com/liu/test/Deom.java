8046bc43aebce1d2923b0e0ee0eb57fb9e13f0eb
woshiliuan <514367261@qq.com>
2018-09-22 12:23:46 +0800

    testtest

--- src/com/liu/test/Deom.java
+++ src/com/liu/test/Deom.java
@@ -10,7 +10,7 @@ public class Deom {
         System.out.println("three");
         "test".substring(0,1);
         "good".substring(0,1);
-        System.out.println("six");
+        System.out.println("five");
     }
 
     public static void f2() {
