da037533d46e5131cdc93961d6ca40172ce8832e
woshiliuan <514367261@qq.com>
2018-12-22 15:18:08 +0800

    Merge remote-tracking branch 'remotes/origin/branch11'
    # Conflicts:
    #	src/com/liu/test/Deom.java
    
    delete one line
    # Conflicts:
    #	src/com/liu/test/Deom.java
    
    delete one line
    # Conflicts:
    #	src/com/liu/test/Deom.java

--- src/com/liu/test/Deom.java
+++ src/com/liu/test/Deom.java
@@ -23,9 +23,5 @@ public class Deom {
     public static void main(String[] args) {
         f1();
         f2();
-        System.out.println(111);
-        System.out.println(222);
-        System.out.println(333);
-        System.out.println(444);
     }
 }
