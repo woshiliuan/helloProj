bb7eed3b7b300cae559efb7c35c2daf9c23bb27a
woshiliuan <514367261@qq.com>
2018-12-22 17:08:55 +0800

    delete some line # Conflicts: #	src/com/liu/test/Deom.java
    delete some line
    # Conflicts:
    #	src/com/liu/test/Deom.java

--- src/com/liu/test/Deom.java
+++ src/com/liu/test/Deom.java
@@ -5,6 +5,9 @@ package com.liu.test;
  */
 public class Deom {
     public static  void f1() {
+        System.out.println(99999);
+        System.out.println(88888);
+        System.out.println(77777);
     }
 
     public static void f2() {
