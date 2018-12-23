7057ccd822160f39b9f00e330feb2be0b656545f
robin <514367261@qq.com>
2018-12-23 13:21:02 +0800

    remote master add ttt
    remote master add ttt

--- src/com/liu/test/Deom.java
+++ src/com/liu/test/Deom.java
@@ -14,17 +14,6 @@ public class Deom {
     }
 
     public static void main(String[] args) {
-        f1();
-        f2();
-        System.out.println("remote change 1");
-        System.out.println("cccc");
-        System.out.println("remote master add 2");
-        System.out.println("local master add tt");
-        System.out.println("remote master add Deom something1");
-        System.out.println("local master add Deom sm1");
-        System.out.println("remote master add Deom something2");
-        System.out.println("local master add Deom sm2");
-        System.out.println("remote master add Deom something3");
-        System.out.println("local master add Deom sm3");
+        System.out.println("remote master add ttt")
     }
 }
