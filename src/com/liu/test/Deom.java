efa74ef94455bf42dff828b6cecca0d3a1ab20b8
woshiliuan <514367261@qq.com>
2018-12-23 15:00:22 +0800

    Merge branch 'master' of https://github.com/woshiliuan/helloProj
    # Conflicts:
    #	src/com/liu/test/Deom.java

--- src/com/liu/test/Deom.java
+++ src/com/liu/test/Deom.java
@@ -14,6 +14,6 @@ public class Deom {
     }
 
     public static void main(String[] args) {
-        System.out.println("remote master add ttt")
+        System.out.println("remote master add ttt");
     }
 }
