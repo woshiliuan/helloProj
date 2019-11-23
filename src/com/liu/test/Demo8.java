d46c89345d1c9127315dd2309e1571eb49fc0bcf
woshiliuan <514367261@qq.com>
2019-11-23 16:33:40 +0800

    增加BB

--- src/com/liu/test/Demo8.java
+++ src/com/liu/test/Demo8.java
@@ -5,5 +5,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Demo8 {
     public static void main(String[] args) {
         System.out.println("AAA");
+        System.out.println("BBB");
     }
 }
