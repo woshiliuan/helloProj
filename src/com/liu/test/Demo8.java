fd6b4bda042e629ecc21a9e3b2e489f13c08362e
woshiliuan <514367261@qq.com>
2019-11-23 17:10:02 +0800

    bbb at local

--- src/com/liu/test/Demo8.java
+++ src/com/liu/test/Demo8.java
@@ -4,9 +4,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Demo8 {
     public static void main(String[] args) {
-        System.out.println("aaa at local");
-        System.out.println("bbb at local");
-        System.out.println("111 at github");
-        System.out.println("222 at github");
+
     }
 }
