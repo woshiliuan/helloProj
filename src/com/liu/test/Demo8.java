0a8603912c3f316f41d183d08cc51a5f0075aa70
woshiliuan <514367261@qq.com>
2019-11-23 17:11:38 +0800

    111 at locals

--- src/com/liu/test/Demo8.java
+++ src/com/liu/test/Demo8.java
@@ -4,6 +4,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Demo8 {
     public static void main(String[] args) {
-        System.out.println("111 at locals");
+
     }
 }
