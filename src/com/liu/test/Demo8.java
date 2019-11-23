caa99249c2e11c929a13526e20020c8f70b8c94d
woshiliuan <514367261@qq.com>
2019-11-23 16:59:31 +0800

    增加EEE

--- src/com/liu/test/Demo8.java
+++ src/com/liu/test/Demo8.java
@@ -4,10 +4,6 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 
 public class Demo8 {
     public static void main(String[] args) {
-        System.out.println("AAA");
-        System.out.println("BBB");
-        System.out.println("111111");
-        System.out.println("CCC");
-        System.out.println("DDD");
+
     }
 }
